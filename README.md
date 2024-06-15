4187871f53dc0d0d8ec52b44f824b93273614f83
Nitya <niittss27@gmail.com>
2024-06-16 02:39:43 +0530

    Nav bar styling completed

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+ ### Self AI BOT
+ - 🌐 An AI for your daily life.
+ -  Built using js, html, css, react.
+ - Demo: bot-for-self.netlify.app
+ - Code: https://github.com/Nitarya/Self-Bot.git
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import './Main.css'
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
 
 const Main = () => {
-    return (
-        <div className='main'>
-
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you today?</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
