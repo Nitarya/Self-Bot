764cf0ce33b2f1643bb8f3455bc3d0cfb39398dc
Nitya <niittss27@gmail.com>
2024-06-15 10:39:04 +0530

    Added css for sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -14,12 +14,25 @@ const Sidebar = () => {
         <div className="recent">
           <p className="recent-title">Recent</p>
           <div className="recent-entry">
-            <img rc={assets.message_icon} alt="" />
+            <img src={assets.message_icon} alt="" />
             <p>What is react...</p>
           </div>
         </div>
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Settings</p>
+        </div>
+      </div>
     </div>
   );
 };
